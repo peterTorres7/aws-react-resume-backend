d185376e37b64d6e6914c10f71bfdb0f5bce8b01
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-03-13 17:58:47 -0400

    added bucket policy

--- s3.tf
+++ s3.tf
@@ -11,26 +11,26 @@ resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
   restrict_public_buckets = false
 }
 
-# resource "aws_s3_bucket_policy" "bucket_policy" {
-#     bucket = aws_s3_bucket.website_bucket.bucket
-#     policy = data.aws_iam_policy_document.allow_access.json
-# }
+resource "aws_s3_bucket_policy" "bucket_policy" {
+    bucket = aws_s3_bucket.website_bucket.bucket
+    policy = data.aws_iam_policy_document.allow_access.json
+}
 
-# data "aws_iam_policy_document" "allow_access" {
-#     statement {
-#         principals {
-#             type = "AWS"
-#             identifiers = ["*"]
-#         }
-#         actions = [
-#             "s3:GetObject",
-#         ]
-#         resources = [
-#             aws_s3_bucket.website_bucket.arn,
-#             "${aws_s3_bucket.website_bucket.arn}/*",
-#         ]
-#     }
-# }
+data "aws_iam_policy_document" "allow_access" {
+    statement {
+        principals {
+            type = "AWS"
+            identifiers = ["*"]
+        }
+        actions = [
+            "s3:GetObject",
+        ]
+        resources = [
+            aws_s3_bucket.website_bucket.arn,
+            "${aws_s3_bucket.website_bucket.arn}/*",
+        ]
+    }
+}
 
 resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
   bucket = aws_s3_bucket.website_bucket.bucket
