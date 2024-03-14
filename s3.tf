c17354e962e463659af2b578c2db42e0401a8d4d
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-03-14 11:23:23 -0400

    added bucket ownership control

--- s3.tf
+++ s3.tf
@@ -2,6 +2,13 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "bucket_controls" {
+    bucket = aws_s3_bucket.website_bucket.id
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
 resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
   bucket = aws_s3_bucket.website_bucket.id
 
