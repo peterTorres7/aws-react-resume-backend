99ab5cf646ef7e9305798e9283ef5be10d0fa0df
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-03-14 12:08:41 -0400

    added bucket policy

--- s3.tf
+++ s3.tf
@@ -28,25 +28,25 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
     acl = "public-read"
 }
 
-# resource "aws_s3_bucket_policy" "bucket_policy" {
-#     bucket = aws_s3_bucket.website_bucket.bucket
-#     policy = data.aws_iam_policy_document.allow_access.json
-# }
+resource "aws_s3_bucket_policy" "bucket_policy" {
+    bucket = aws_s3_bucket.website_bucket.bucket
+    policy = data.aws_iam_policy_document.allow_access.json
+}
 
-# data "aws_iam_policy_document" "allow_access" {
-#     statement {
-#         principals {
-#             type = "AWS"
-#             identifiers = ["*"]
-#         }
-#         actions = [
-#             "s3:GetObject",
-#         ]
-#         resources = [
-#             "${aws_s3_bucket.website_bucket.arn}/*",
-#         ]
-#     }
-# }
+data "aws_iam_policy_document" "allow_access" {
+    statement {
+        principals {
+            type = "AWS"
+            identifiers = ["*"]
+        }
+        actions = [
+            "s3:GetObject",
+        ]
+        resources = [
+            "${aws_s3_bucket.website_bucket.arn}/*",
+        ]
+    }
+}
 
 # resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
 #   bucket = aws_s3_bucket.website_bucket.bucket
