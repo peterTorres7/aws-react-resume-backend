a31859000028cba6d0ee38d9350809eea10c0b3a
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-03-14 11:33:10 -0400

    recreate bucket

--- s3.tf
+++ s3.tf
@@ -8,40 +8,40 @@ resource "aws_s3_bucket" "website_bucket" {
 #         object_ownership = "BucketOwnerPreferred"
 #     }
 # }
-#
-resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
-  bucket = aws_s3_bucket.website_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+#   bucket = aws_s3_bucket.website_bucket.id
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-    bucket = aws_s3_bucket.website_bucket.bucket
-    policy = data.aws_iam_policy_document.allow_access.json
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-data "aws_iam_policy_document" "allow_access" {
-    statement {
-        principals {
-            type = "AWS"
-            identifiers = ["*"]
-        }
-        actions = [
-            "s3:GetObject",
-        ]
-        resources = [
-            "${aws_s3_bucket.website_bucket.arn}/*",
-        ]
-    }
-}
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#     bucket = aws_s3_bucket.website_bucket.bucket
+#     policy = data.aws_iam_policy_document.allow_access.json
+# }
 
-resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
-  bucket = aws_s3_bucket.website_bucket.bucket
+# data "aws_iam_policy_document" "allow_access" {
+#     statement {
+#         principals {
+#             type = "AWS"
+#             identifiers = ["*"]
+#         }
+#         actions = [
+#             "s3:GetObject",
+#         ]
+#         resources = [
+#             "${aws_s3_bucket.website_bucket.arn}/*",
+#         ]
+#     }
+# }
 
-  index_document {
-    suffix = "index.html"
-  }
-}
+# resource "aws_s3_bucket_website_configuration" "website_bucket_config" {
+#   bucket = aws_s3_bucket.website_bucket.bucket
+
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
