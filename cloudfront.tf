9735b3be7af9b16ef4b87d0d33688c2bdd2a96b9
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-07 11:35:26 -0400

    changed domain name

--- cloudfront.tf
+++ cloudfront.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
     origin {
-      domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+      domain_name = aws_s3_bucket.website_bucket.website_endpoint
       origin_access_control_id = aws_cloudfront_origin_access_control.default.id
       origin_id = local.s3_origin_id
     }
