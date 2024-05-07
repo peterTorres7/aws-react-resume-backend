37361461fa4c132bde97d996a00c2b1e2f04f7f1
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-07 19:52:04 -0400

    replaced origin id

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,10 +1,11 @@
 locals {
-    s3_origin_id = "${aws_s3_bucket.website_bucket.bucket}OAI"
+    s3_origin_id = "${aws_s3_bucket.website_bucket.bucket}.s3-website-us-east-1.amazonaws.com"
+
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
     origin {
-      domain_name = aws_s3_bucket.website_bucket.website_endpoint
+      domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
       origin_access_control_id = aws_cloudfront_origin_access_control.default.id
       origin_id = local.s3_origin_id
     }
@@ -12,14 +13,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     enabled = true
     is_ipv6_enabled = true
     default_root_object = "index.html"
-
-    # logging_config {
-    #     include_cookies = false
-    #     bucket = "mylogs.s3.amazonaws.com"
-    #     prefix = "myprefix"
-    # }
-
     aliases = ["www.petertorres.link"]
+    wait_for_deployment = true
 
     default_cache_behavior {
         allowed_methods = ["GET", "HEAD"]
@@ -40,28 +35,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         max_ttl = 31536000
     }
 
-    # ordered_cache_behavior {
-    #     path_pattern = "/*"
-    #     allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    #     cached_methods = ["GET", "HEAD", "OPTIONS"]
-    #     target_origin_id = local.s3_origin_id
-
-    #     forwarded_values {
-    #         query_string = false
-    #         headers = ["Origin"]
-
-    #         cookies {
-    #             forward = "none"
-    #         }
-    #     }
-
-    #     min_ttl = 0
-    #     default_ttl = 86400
-    #     max_ttl = 31536000
-    #     compress = true
-    #     viewer_protocol_policy = "redirect-to-https"
-    # }
-
     # price_class = "PriceClass200"
 
     restrictions {
@@ -70,10 +43,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
             locations = ["US", "CA", "GB", "DE"]
         }
     }
-
-    # tags = {
-    #     Environment = "development"
-    # }
 
     viewer_certificate {
         cloudfront_default_certificate = true
