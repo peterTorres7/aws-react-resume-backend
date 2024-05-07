9df12950f6300a49e37e3ea0f3bb03a2202ca61b
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-07 10:44:00 -0400

    updated minimum protocol version

--- cloudfront.tf
+++ cloudfront.tf
@@ -77,6 +77,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_certificate {
         cloudfront_default_certificate = true
+        minimum_protocol_version = "TLSv1.2_2021"
         acm_certificate_arn = "arn:aws:acm:us-east-1:243481346369:certificate/a4108c08-5bfe-413e-b5a6-931388bdd390"
         ssl_support_method = "sni-only"
     }
