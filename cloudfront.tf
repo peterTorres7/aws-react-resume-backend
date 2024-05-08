d2f01d6d78da0a73c9a497172e0f0f8ccf9c3c07
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-08 08:54:28 -0400

    changed origin id

--- cloudfront.tf
+++ cloudfront.tf
@@ -5,9 +5,9 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
     origin {
-      domain_name = aws_s3_bucket.website_bucket.website_endpoint
+      domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
       origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-      origin_id = local.s3_origin_id
+      origin_id = aws_s3_bucket.website_bucket.website_endpoint
     }
 
     enabled = true
