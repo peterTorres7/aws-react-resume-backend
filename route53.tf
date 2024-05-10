ccd2578a38ad1456f8faf228aa0227f18601a371
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-10 09:54:21 -0400

    updated alias name

--- route53.tf
+++ route53.tf
@@ -16,7 +16,7 @@ resource "aws_route53_record" "reroute" {
   type = "A"
   
   alias {
-    name = "www.petertorres.link"
+    name = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
