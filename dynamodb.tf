5ba1752e0d1e6c2b63d5cf81f59548b35a2ae460
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-18 22:10:14 -0400

    added read and write autoscaling

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "visits_table" {
   name = "visits_table"
   billing_mode = "PROVISIONED"
-  read_capacity = 10
-  write_capacity = 10
+  read_capacity = 1
+  write_capacity = 1
   hash_key = "id"
 
   attribute {
@@ -10,8 +10,55 @@ resource "aws_dynamodb_table" "visits_table" {
     type = "S"
   }
 
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled = false
+  lifecycle {
+  ignore_changes = [read_capacity, write_capacity]
   }
+}
+
+resource "aws_appautoscaling_target" "visits_table_read_target" {
+  max_capacity = 10
+  min_capacity = 1
+  resource_id = "table/visits_table"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  service_namespace = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "visits_table_read_policy" {
+  name = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.visits_table_read_target.resource_id}"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.visits_table_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.visits_table_read_target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.visits_table_read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70.0
+  }
+}
+
+resource "aws_appautoscaling_target" "visits_table_write_target" {
+  max_capacity = 10
+  min_capacity = 1
+  resource_id = "table/visits_table"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  service_namespace = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "visits_table_write_policy" {
+  name = "DynampDBWriteCapacityUtilization:${aws_appautoscaling_target.visits_table_write_target}"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.visits_table_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.visits_table_write_target.scalable_dimension
+  service_namespace = aws_appautoscaling_target.visits_table_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriceCapacityUtilization"
+    }
+
+    target_value = 70.0
+  } 
 }
