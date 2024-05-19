10250482c2a9cc716d7edad13feec2acab2ff2dc
Peter Torres <74520612+peterTorres7@users.noreply.github.com>
2024-05-18 22:21:39 -0400

    fixed table policy

--- dynamodb.tf
+++ dynamodb.tf
@@ -48,7 +48,7 @@ resource "aws_appautoscaling_target" "visits_table_write_target" {
 }
 
 resource "aws_appautoscaling_policy" "visits_table_write_policy" {
-  name = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.visits_table_write_target}"
+  name = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.visits_table_write_target.resource_id}"
   policy_type = "TargetTrackingScaling"
   resource_id = aws_appautoscaling_target.visits_table_write_target.resource_id
   scalable_dimension = aws_appautoscaling_target.visits_table_write_target.scalable_dimension
